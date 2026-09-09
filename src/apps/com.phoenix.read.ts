d126b628bd67df14580f5b72ea923267a94ea09f
yangyuguo <yangyuguo2011@gmail.com>
2026-09-09 19:28:21 +0800

    1

--- src/apps/com.phoenix.read.ts
+++ src/apps/com.phoenix.read.ts
@@ -26,31 +26,10 @@ export default defineGkdApp({
           },
           activityIds:
             'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
-          matches: '[text="上滑继续观看短剧"][visibleToUser=true]',
+          matches: '[text="上滑继续看短剧"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/29092674',
           excludeSnapshotUrls: 'https://i.gkd.li/i/29092652', //倒计时未结束
           exampleUrls: 'https://e.gkd.li/36e57d51-d134-4507-9aca-ada9e71b9b14',
-        },
-        {
-          key: 2,
-          name: '②4.5秒[上滑]1次',
-          actionCd: 4500,
-          // forcedTime: 60000,
-          swipeArg: {
-            start: {
-              x: 'screenWidth/2',
-              y: 'screenHeight * 0.7',
-            },
-            end: {
-              x: 'screenWidth/2',
-              y: 'screenHeight * 0.3',
-            },
-            duration: 200,
-          },
-          activityIds:
-            'com.dragon.read.component.shortvideo.impl.fullscreen.ShortSeriesLandActivity', //横屏
-          matches: '[visibleToUser=true][desc="广告"]',
-          snapshotUrls: 'https://i.gkd.li/i/29092652', //倒计时未结束
         },
       ],
     },
