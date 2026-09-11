2713335df82eace5a7d3a83726a0742ca7dc7a21
yangyuguo <yangyuguo2011@gmail.com>
2026-09-11 19:19:03 +0800

    1

--- src/apps/com.phoenix.read.ts
+++ src/apps/com.phoenix.read.ts
@@ -10,22 +10,21 @@ export default defineGkdApp({
       desc: '①读秒结束后[上滑] ②4.5秒[上滑]1次',
       rules: [
         {
-          // ② 直播/视频广告：无集数节点时上滑（文字画在视频上，无障碍读不到）
           key: 1,
-          name: '②无集数节点时上滑',
+          name: '②无集数文本时上滑',
           fastQuery: true,
+          matchRoot: true,
           actionCd: 6000,
           activityIds:
             'com.dragon.read.component.shortvideo.impl.ShortSeriesActivity',
-          matches: '[vid="f1b"]',
-          excludeMatches: '[vid="k6n"][text~="第.+集"]',
+          matches: '[name="android.view.View"][visibleToUser=true]',
+          excludeMatches: '[text~="第.+集"]',
           action: 'swipe',
           swipeArg: {
             start: { x: 'screenWidth/2', y: 'screenHeight*0.7' },
             end: { x: 'screenWidth/2', y: 'screenHeight*0.3' },
             duration: 200,
           },
-          snapshotUrls: ['https://i.gkd.li/i/32098613'],
         },
       ],
     },
